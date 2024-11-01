38da703ea05de5362817859d0cdff5ab7ded32da
xelilovkamran <xlilovkamran@gmail.com>
2024-11-01 08:30:29 +0400

    initial commit

---
+++ src/script.js
@@ -0,0 +1,41 @@
+let historyDisplay = document.getElementById("history");
+let resultDisplay = document.getElementById("result");
+let currentInput = "";
+let history = "";
+
+function appendNumber(number) {
+  if (currentInput === "0" && number === "0") return;
+  currentInput += number;
+  resultDisplay.innerText = currentInput;
+}
+
+function appendOperator(operator) {
+  if (currentInput === "") return;
+  history += currentInput + " " + operator + " ";
+  currentInput = "";
+  historyDisplay.innerText = history;
+}
+
+function clearDisplay() {
+  currentInput = "";
+  history = "";
+  resultDisplay.innerText = "0";
+  historyDisplay.innerText = "";
+}
+
+function calculateResult() {
+  history += currentInput;
+  historyDisplay.innerText = history;
+  try {
+    resultDisplay.innerText = eval(history);
+  } catch (error) {
+    resultDisplay.innerText = "Error";
+  }
+  history = "";
+  currentInput = resultDisplay.innerText;
+}
+
+function deleteLast() {
+  currentInput = currentInput.slice(0, -1);
+  resultDisplay.innerText = currentInput || "0";
+}
